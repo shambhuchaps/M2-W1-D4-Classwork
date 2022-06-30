c63d167313f4b6d9745245b220bd82426bd31f67
shambhu <chapagainshambhu56@gmail.com>
2022-06-30 16:23:23 +0545

    This is my M2 W1 D4 classwork.

---
+++ index-script.js
@@ -0,0 +1,9 @@
+/*var element_header = document.createElement('header');
+var element_h1 = document.createElement('h1');
+var text_h1 = document.TEXT_NODE('This is H1 tag');
+var test = document.querySelector('#test');
+test.appendChild(element_h1);
+element_header.appendChild(element_h1);
+document.body.appendChild(element_header);
+document.body.appendChild(text_h1)*/
+
